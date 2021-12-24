83a9f7795d07992b1cb9e700a88cfc20fc66df63
Rhea Dsouza <rheadqs@gmail.com>
2021-12-24 23:52:39 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,8 @@
+function lengthConverter1(valNum) 
+{
+    document.getElementById("outputMeters").innerHTML = valNum / 0.032808;
+  }
+  function lengthConverter2(valNum) 
+  {
+    document.getElementById("outputCentimeters").innerHTML = valNum / 30.48;
+  }
