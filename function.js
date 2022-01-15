e47ee90948789737bd05bb296b22dd2516b22b7d
Rhea Dsouza <rheadqs@gmail.com>
2022-01-15 22:36:06 +0530

    Website

--- function.js
+++ function.js
@@ -1,8 +1,8 @@
 function lengthConverter1(Num) 
 {
-    document.getElementById("outputCentimeter").innerHTML = Num / 0.032808;
-  }
-  function lengthConverter2(Num) 
-  {
-    document.getElementById("outputFeet").innerHTML = Num / 30.48;
-  }
+  document.getElementById("outputCentimeter").innerHTML = Num / 0.032808;
+}
+function lengthConverter2(Num) 
+{
+  document.getElementById("outputFeet").innerHTML = Num / 30.48;
+}
